1b193ffec333d5cfc1e8bf1d6e157bcdccf68282
Igor Andrade <oigorwebs@gmail.com>
2024-01-25 20:40:35 -0300

    Get SECRET_KEY from .env file

--- api_service/settings/base.py
+++ api_service/settings/base.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h@zs&o7ul__lo_up(qqgjd%==p=yw5q0@ksjxmkae=(4o#_k=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
