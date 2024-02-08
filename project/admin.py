354d97bdd7c5433ceafa229586893beacc9ac3a5
Igor Andrade <oigorwebs@gmail.com>
2024-02-08 11:57:11 -0300

    Delete app

--- project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Project, SequencingMethod, Country
-
-admin.site.register(Project)
-admin.site.register(SequencingMethod)
-admin.site.register(Country)
