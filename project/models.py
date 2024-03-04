22c05c3f6017d7a65dad1205b8824535df8df4b6
Igor Andrade <oigorwebs@gmail.com>
2024-03-04 15:42:32 -0300

    Add SequencingReadType model

--- project/models.py
+++ project/models.py
@@ -15,13 +15,17 @@ class Country(models.Model):
         return self.name
 
 
+# Sequencing Read Types
+class SequencingReadType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 # Sequencing Method
 class SequencingMethod(models.Model):
     name = models.CharField(max_length=100)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -32,6 +36,9 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="studies")
     sequencing_method = models.ForeignKey(SequencingMethod, on_delete=models.PROTECT)
+    sequencing_read_type = models.ForeignKey(
+        SequencingReadType, on_delete=models.PROTECT
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
