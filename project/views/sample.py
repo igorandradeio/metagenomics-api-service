44badcb1715184668970b187ef0da112be8e7e04
Igor Andrade <oigorwebs@gmail.com>
2024-03-14 16:24:51 -0300

    Remove unused module

--- project/views/sample.py
+++ project/views/sample.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
