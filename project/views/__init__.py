323ba3a3a386068d62f8cc18dd252e61fbc78f77
Igor Andrade <oigorwebs@gmail.com>
2024-04-26 00:50:09 -0300

    Rename class

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,5 +1,5 @@
 from .project import ProjectViewSet
-from .sample import SampleUploadViewSet
+from .sample import SampleViewSet
 from .assembly import AssemblyUploadViewSet
 from .sequencing_method import SequencingMethodViewSet
 from .sequencing_read_type import SequencingReadTypeViewSet
