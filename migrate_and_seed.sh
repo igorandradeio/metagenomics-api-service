5d3864ab1465fc9466a219d8c2a7d600368bd998
Igor Andrade <oigorwebs@gmail.com>
2024-06-01 01:29:44 -0300

    feat: add script for database migrations and seeding

--- migrate_and_seed.sh
+++ migrate_and_seed.sh
@@ -1,8 +1,15 @@
 # !/bin/bash
-python3 manage.py migrate
+
+# Generate migrations
+poetry run python manage.py makemigrations
+
+# Run migrations
+poetry run python manage.py migrate
+
+# Run seeders 
 fixtures=$(ls seed/)
 while IFS= read -r fixture; do
     echo -n "Seeding "
     echo $fixture
-    python3 manage.py loaddata seed/$fixture
+    poetry run python3 manage.py loaddata seed/$fixture
 done <<< "$fixtures"
