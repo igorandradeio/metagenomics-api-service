55d5334cd723c4d60a7164bedcaa8423c7a6d686
Igor Andrade <oigorwebs@gmail.com>
2024-11-25 01:23:52 -0300

    feature: annotation

---
+++ project/views/annotation.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from project.tasks import run_annotation
+from project.models import Project
+from task.models import Task, TaskStatus
+from django.shortcuts import get_object_or_404
+
+
+class AnnotationViewSet(viewsets.ViewSet):
+
+    @action(detail=True, methods=["post"])
+    def start_annotation(self, request, pk):
+        user = request.user
+        project = get_object_or_404(Project, pk=pk, user=user)
+        samples = project.samples.all()
+
+        if not samples.exists():
+            return Response(
+                {"error": "No samples found for the project."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        # Extract only the file names from the samples
+        input_files = [sample.file_name for sample in samples]
+
+        sequencing_read_type = project.sequencing_read_type_id
+
+        # Start the Celery task
+        task = run_annotation.delay(
+            pk, sequencing_read_type, input_files, user.id)
+
+        task_id = task.id
+
+        # Save the initial task status as pending
+        task = Task(user=user, task_id=task_id, type=1,
+                    project=project, status=TaskStatus.PENDING)
+        task.save()
+
+        return Response({"task_id": task_id}, status=status.HTTP_200_OK)
